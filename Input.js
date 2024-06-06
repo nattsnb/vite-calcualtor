25f92689e89dca19c2b224ef786912ccc57bbbac
Natasza ASzatan <natiye.nb@gmai.com>
2024-06-06 22:43:13 +0100

    finished functionality of the calc

--- Input.js
+++ Input.js
@@ -10,22 +10,28 @@ export class Input {
     }
     eqButtonPressed = (eq) => {
         if(eq === "="){
+            this.num2 = Number(this.inputContainer.innerHTML.substring(3))
             const result = this.doTheEq()
+            console.log(this.num2)
+            console.log("=")
             console.log(result)
-            this.inputContainer.innerHTML = ""
+            this.inputContainer.innerHTML = result
             this.num1 = null
             this.num2 = null
             this.eqSign = null
         } else {
             if (this.num1 === null) {
                 this.num1 = Number(this.inputContainer.innerHTML)
-                this.inputContainer.innerHTML = null
+                this.inputContainer.innerHTML = `${eq}  `
                 this.eqSign = eq;
                 console.log(this.num1)
                 console.log(eq)
             } else {
-                this.num2 = Number(this.inputContainer.innerHTML)
+                this.num2 = Number(this.inputContainer.innerHTML.substring(3))
                 console.log(this.num2)
+                console.log("=")
+                this.num1 = this.doTheEq()
+                console.log(this.num1)
                 console.log(eq)
             }
         }
